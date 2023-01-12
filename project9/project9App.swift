363dff977c2d62718e7a7ef9d5df223ab0581cac
Tyson Walker II <tywalk10@gmail.com>
2023-01-12 04:31:57 -0500

    Initial Commit

---
+++ project9/project9App.swift
@@ -0,0 +1,17 @@
+//
+//  project9App.swift
+//  project9
+//
+//  Created by Tyson Laroyce Walker II on 10/31/22.
+//
+
+import SwiftUI
+
+@main
+struct project9App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
